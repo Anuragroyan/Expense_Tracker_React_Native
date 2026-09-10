e24d9ae9ff25f9a06a3b99b234828e0903c7640d
Anuragroyan <7619354965anurag@gmail.com>
2026-09-10 21:31:17 +0530

    Initial commit: Set up Expense Tracker Validation app

---
+++ screens/ManageExpenses.js
@@ -0,0 +1,75 @@
+import { View, StyleSheet } from "react-native";
+import { useContext, useLayoutEffect } from "react";
+import IconButton from "../components/UI/IconButton";
+import { GlobalColors } from "../constants/color";
+import { ExpensesContext } from "../store/expenses-context";
+import ExpenseForm from "../components/ManageExpense/ExpenseForm";
+
+function ManageExpenses({ route, navigation }) {
+  const expensesCtx = useContext(ExpensesContext);
+  const editedExpenseId = route.params?.expenseId;
+  const isEditing = !!editedExpenseId;
+  const selectedExpense = expensesCtx.expenses.find(
+    (expense) => expense.id === editedExpenseId
+  );
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: isEditing ? "Edit Expense" : "Add Expense",
+    });
+  }, [navigation, isEditing]);
+
+  function deleteExpenseHandler() {
+    expensesCtx.deleteExpense(editedExpenseId);
+    navigation.goBack();
+  }
+  function cancelHandler() {
+    navigation.goBack();
+  }
+  function confirmHandler(expenseData) {
+    if (isEditing) {
+      expensesCtx.updateExpense(editedExpenseId, expenseData);
+    } else {
+      expensesCtx.addExpense(expenseData);
+    }
+    navigation.goBack();
+  }
+
+  return (
+    <View style={styles.container}>
+      <ExpenseForm
+        submitButtonLabel={isEditing ? "Update" : "Add"}
+        onSubmit={confirmHandler}
+        onCancel={cancelHandler}
+        defaultValues={selectedExpense}
+      />
+      {isEditing && (
+        <View style={styles.deleteContainer}>
+          <IconButton
+            icon="trash"
+            color={GlobalColors.colors.error500}
+            size={36}
+            onPress={deleteExpenseHandler}
+          />
+        </View>
+      )}
+    </View>
+  );
+}
+
+export default ManageExpenses;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+    backgroundColor: GlobalColors.colors.primary800,
+  },
+  deleteContainer: {
+    marginTop: 16,
+    paddingTop: 8,
+    borderTopWidth: 2,
+    borderTopColor: GlobalColors.colors.primary200,
+    alignItems: "center",
+  },
+});
